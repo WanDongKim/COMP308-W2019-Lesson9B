8f1abac686a9243038222224e9653e95d13e2945
Dongwan Kim <zeumes12@gmail.com>
2019-03-23 14:23:06 -0400

    added Add Component Functionality - closes #1

--- client/src/app/contacts/contact-details/contact-details.component.ts
+++ client/src/app/contacts/contact-details/contact-details.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactListService } from 'src/app/services/contact-list.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router, ActivatedRoute } from '@angular/router';
 
+
+import { Contact } from 'src/app/models/contact';
 @Component({
   selector: 'app-contact-details',
   templateUrl: './contact-details.component.html',
   styleUrls: ['./contact-details.component.css']
 })
 export class ContactDetailsComponent implements OnInit {
+  title:string;
+  contact: Contact;
 
-  constructor() { }
+  constructor(
+    private activedRoute:ActivatedRoute,
+    private flashMessage:FlashMessagesService,
+    private contactListService: ContactListService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.title = this.activedRoute.snapshot.data.title;
+    this.contact = new Contact();
   }
+  private onDetailsPageSubmit():void{
+    switch(this.title){
+      case 'Add Contact':
+      this.contactListService.addContact(this.contact).subscribe(data => {
+        if(data.success){
+          this.flashMessage.show(data.msg, {cssClass: 'alert-success', timeOut: 3000});
+          this.router.navigate(['/contact/contact-list']);
+        } else {
+          this.flashMessage.show('Add Contact Failed', {cssClass: 'alert-danger', timeOut: 3000});
+        }
+      });
+      break;
 
+      case 'Edit Contact':
+      break;
+    }
+  }
 }
--- client/src/app/services/contact-list.service.ts
+++ client/src/app/services/contact-list.service.ts
@@ -3,13 +3,15 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Contact } from '../models/contact';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactListService {
+  private user:User;
 
-  private endpoint = "http://localhost:3000/api/contact-list";
+  private endpoint = "http://localhost:3000/api/contact-list/";
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -23,4 +25,19 @@ export class ContactListService {
     return this.http.get<any>(this.endpoint,this.httpOptions);
   }
 
+  public getContact(contact:Contact):Observable<any>{
+    return this.http.get<any>(this.endpoint + 'edit/' + contact._id, this.httpOptions);
+  }
+
+  public addContact(contact:Contact):Observable<any>{
+    return this.http.post<any>(this.endpoint + 'add', contact, this.httpOptions);
+  }
+
+  public editContact(contact:Contact):Observable<any>{
+    return this.http.post<any>(this.endpoint + 'edit/' + contact._id, contact, this.httpOptions);
+  }
+
+  public deleteContact(contact:Contact):Observable<any>{
+    return this.http.get<any>(this.endpoint + 'delete/' + contact._id, this.httpOptions);
+  }
 }
