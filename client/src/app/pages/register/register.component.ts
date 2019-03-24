ea90da750ca5784522495d84e2ee0e02e763be93
Dongwan Kim <zeumes12@gmail.com>
2019-03-23 20:29:41 -0400

    added Register and Login - closes #4 and closes#5

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,4 +1,9 @@
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 import { Component, OnInit } from '@angular/core';
+
+import { User } from './../../models/user';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+    private flashMessage:FlashMessagesService,
+    private authService:AuthService,
+    private router:Router
+  ) { }
 
   ngOnInit() {
+    this.user = new User();
   }
 
+  onRegisterSubmit():void{
+    this.authService.registerUser(this.user).subscribe(data =>{
+      if(data.success){
+        this.flashMessage.show('You are now registered and may log in', {cssClass: 'alert-success', timeOut: 3000});
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessage.show('A registration Error Occurred', {cssClass: 'alert-danger', timeOut: 3000});
+        this.router.navigate(['/register']);
+      }
+    });
+  }
 }
